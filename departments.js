74c45bfc761578201441dd38f877fc9669c7f5b3
physicstjc <85329791+physicstjc@users.noreply.github.com>
2025-03-19 10:38:01 +0800

    Add files via upload

---
+++ departments.js
@@ -0,0 +1,17 @@
+const departments = {
+    'AA': 'Arts Aesthetics',
+    'AE': 'Arts Economics',
+    'AG': 'Arts Geography',
+    'AH': 'Arts History',
+    'AM': 'Arts Music',
+    
+    // Other Departments
+    'E': 'English',
+    'M': 'Mathematics',
+    'MT': 'Mother Tongue Languages',
+    'PE': 'Physical Education',
+    'PW': 'Project Work',
+    'SB': 'Science Biology',
+    'SC': 'Science Chemistry',
+    'SP': 'Science Physics'
+};
