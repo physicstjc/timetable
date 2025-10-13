44a1078f716ebf7af24dfc05a318c71247dfc566
physicstjc <85329791+physicstjc@users.noreply.github.com>
2025-10-13 13:30:34 +0800

    Update compare.js

--- compare.js
+++ compare.js
@@ -16,7 +16,7 @@ let savedGroups = JSON.parse(localStorage.getItem('teacherGroups') || '{}');
 async function loadDefaultTimetable() {
     try {
         // Try known timetable files inside timetables/ directly (no directory listing)
-        const fallbackFiles = ['term4week4.xml', 'term4week5.xml', 'term4week6-7.xml'];
+        const fallbackFiles = ['term4week5.xml', 'term4week6-7.xml'];
         for (const name of fallbackFiles) {
             try {
                 const res = await fetch(`timetables/${name}`);
@@ -748,7 +748,7 @@ function populateXMLDropdown() {
             if (xmlFiles.length) {
                 xmlFiles.forEach(name => addOption(name));
             } else {
-                ['term4week4.xml', 'term4week5.xml', 'term4week6-7.xml'].forEach(addOption);
+                ['term4week5.xml', 'term4week6-7.xml'].forEach(addOption);
             }
             if (select.options.length) {
                 select.selectedIndex = 0;
@@ -756,7 +756,7 @@ function populateXMLDropdown() {
             }
         })
         .catch(() => {
-            ['term4week4.xml', 'term4week5.xml', 'term4week6-7.xml'].forEach(addOption);
+            ['term4week5.xml', 'term4week6-7.xml'].forEach(addOption);
             if (select.options.length) {
                 select.selectedIndex = 0;
                 loadSelectedXML(select.value);
